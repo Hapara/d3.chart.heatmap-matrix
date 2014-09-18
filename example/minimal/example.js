0a9f646279cff1ba0a15c676c23502b563543a27
leebecker <lee.becker@hapara.com>
2014-09-18 16:42:30 -0600

    Fixed up minimal example

--- example/minimal/example.js
+++ example/minimal/example.js
@@ -2,17 +2,18 @@
 
 var d3 = require('d3');
 var colorbrewer = require('../colorbrewer')
-var chart = require('d3.chart.heatmap');
+var chart = require('d3.chart.heatmap-matrix');
 
 
-var palette = 'RdYlGn'
-var numClasses = 11
+//var palette = 'RdYlGn'
+//var numClasses = 11
 
-var colors = d3.scale.quantize().domain([0,1]).range(colorbrewer[palette][numClasses])
+//var colors = d3.scale.quantize().domain([0,1]).range(colorbrewer[palette][numClasses])
 var chart = d3.select('#vis').append('svg')
-              .chart('Heatmap')
+              .chart('HeatmapMatrix')
               .width(1024).height(512)
-              .colorScale(colors)
+              .rowKey(function(d) { return d.name; })
+              //.colorScale(colors)
 
 var data = require('./data');
 chart.draw(data)
